9c9507104c303118211379a5e5ed3a96e311762d
qweeternity <pelsh2002@gmail.com>
2023-04-14 00:05:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 199;
+            var deviders = new List<int>();
+            var numbers = new List<int>();
+            int devider = 2;
+
+
+            while
+            while (n != 1)
+            {
+                n = devider;
+                if (n % devider == 0)
+                {
+                    n /= devider;
+                    deviders.Add(devider);
+                    devider = 2;
+                }
+                else
+                {
+                    devider++;
+                }
+             }
+
+            if (deviders.Count <= 1)
+            {
+                numbers.Add(devider); 
+            }
+            else
+            {
+                deviders.Clear();
+            }
+
+            Console.WriteLine(string.Join(" + ", numbers));
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
