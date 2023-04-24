21aa8e39c83fcc68c268b5251c880b04900f438a
Maxos2233 <pelsh2002@gmail.com>
2023-04-24 19:08:07 +0300

    testcommit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 0, 12, -33, 6};
+            var copyofi = 0;
+            List<int> numbersMaximum = new List<int>();
+            List<int> numbersMinimal = new List<int>();
+            foreach (var i in numbers)
+            {
+                
+                
+                if (i >= copyofi)
+                {
+                    numbersMaximum.Clear();
+                    numbersMaximum.Add(i);
+                    numbersMaximum.Add(numbers.IndexOf(i));
+                }
+                else
+                {
+                    copyofi = i;
+                    
+                }
+
+                if (i <= copyofi)
+                {
+                    numbersMinimal.Clear();
+                    numbersMinimal.Add(i);
+                    numbersMinimal.Add(numbers.IndexOf(i));
+
+                }
+                else
+                {
+                    copyofi = i;
+                    
+                }
+
+                copyofi = i;
+
+            }
+
+            Console.WriteLine(string.Join(" ", numbersMaximum));
+            Console.WriteLine(string.Join(" ", numbersMinimal));
+
+            Console.ReadKey();
+        }
+    }
+}
